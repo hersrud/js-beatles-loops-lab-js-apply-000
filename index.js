4264e096e8cccb4ff9556153ccdd5e2d8ea0d137
Kristin Hersrud Kopp <khkopp@gmail.com>
2018-10-25 21:45:29 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,9 @@
+function theBeatlesPlay(musicians,instruments){
+  var musicianString;
+  var musicianInstrument [];
+  for (var i=o; i<musicians.length; i++){
+    musicianString =musician[i] + 'plays' instuments[i];
+    musicianInstument[i]=musicianString;
+  }
+  return musicianInstrument;
+}
