7035ee2b03c35fed5c2678a1672135b8bf14d5e6
Kristin Hersrud Kopp <khkopp@gmail.com>
2018-10-25 22:11:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 function theBeatlesPlay(musicians,instruments){
-  var musicianString;
-  var musicianInstrument[];
-  for (var i=o; i<musicians.length; i++){
-    musicianString=musicians[i] + 'plays' + instuments[i];
-    musicianInstument[i]=musicianString;
+  var phrases = [];
+  for (var i=0; i < musicians.length; i++){
+    phrases.push(musicians[i]+"plays"+instruments[i]);
   }
-  return musicianInstrument;
+  return phrases
 }
